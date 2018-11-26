0181ed778c8172eeef2d880e7dff753b0bc14ebe
HugoSilvaSantos <hugosilva_santos@outlook.com>
2018-11-26 13:22:43 +0000

    Linting added

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+
+
 var express = require('express');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + server.address().port);  
+	console.log('Express server listening on port ' + server.address().port);
 });
 
 // routes
-app.get('/', function(req, res) {
-    res.send({'Message':'Entry Point'});
+app.get('/', function (req, res) {
+	res.send({'Message':'Entry Point'});
 });
-app.get('/home', function(req, res) {
-    res.send({'Message':'OK' });
+
+app.get('/home', function (req, res) {
+	res.send({'Message':'OK' });
 });
 
 module.exports = app;
